1ae7033cecd909b56b1f9c0fdd1aa849cefceb17
Bobo <572025842@qq.com>
2022-01-05 21:47:15 +0800

    端口换为8080，配置redis

--- src/test/java/cn/edu/tongji/easygo/EasygoApplicationTests.java
+++ src/test/java/cn/edu/tongji/easygo/EasygoApplicationTests.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class EasygoApplicationTests {
 
-
+    @Resource
+    StringEncryptor stringEncryptor;
 
     @Test
     void contextLoads() {
-
+        String res = stringEncryptor.encrypt("xxx");
+        System.out.println(res);
     }
 }
